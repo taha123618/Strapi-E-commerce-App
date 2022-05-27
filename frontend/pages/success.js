12d91be93bcc9aa88724338df609efad6471c7dd
Taha Ahmed 123618 <tahaahmedanees2@gmail.com>
2022-05-27 14:43:44 -0700

    Add checkout/succes page & integrate payment gaetway

--- frontend/pages/success.js
+++ frontend/pages/success.js
@@ -18,6 +18,7 @@ const success = () => {
           <div className="flex lg:w-2/3 w-full sm:flex-row flex-col mx-auto px-8 sm:space-x-4 sm:space-y-0 space-y-4 sm:px-0 items-end"></div>
         </div>
       </section>
+      <h1>Testing</h1>
     </div>
   );
 };
